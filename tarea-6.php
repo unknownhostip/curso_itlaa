b930ecc196b0c981f890cf585c35f508e2878572
unknownhostip <33305542+unknownhostip@users.noreply.github.com>
2019-08-16 17:16:36 -0400

    Delete tarea-6.php

--- tarea-6.php
+++
@@ -1,9 +0,0 @@
-<?php
- 
-$dom = $_SERVER['HTTP_HOST']; //recuperamos el dominio
-$rest = $_SERVER['REQUEST_URI']; //recuperamos el resto
-$url_completa = "http://" . $dom . $rest; //armamos la url
- 
-echo $url_completa;
- 
-?>
